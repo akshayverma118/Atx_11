95249601d9e5d6d412d7342d080bef60801b2bb8
akshayverma118 <akshayverma118@gmail.com>
2025-03-10 08:29:00 +0530

    First java program revstring

---
+++ src/Program1.java
@@ -0,0 +1,18 @@
+public class Program1 {
+
+    public static void main (String [] args)
+    {
+        String rev = "Akshay";
+        String rev1 = " ";
+        char chr;
+
+
+        for(int i =0; i < rev.length(); i++)
+        {
+            chr = rev.charAt(i);
+            rev1 = chr + rev1;
+        }
+
+        System.out.println("Reverse String is " + rev1);
+    }
+}
