e62a16224565a9b665f03acb710921fd819e784f
akshayverma118 <akshayverma118@gmail.com>
2025-03-11 23:06:04 +0530

    First java program revstring

---
+++ src/ReverseStringarray.java
@@ -0,0 +1,22 @@
+public class ReverseStringarray {
+
+    String a[] = {"India", "Japan", "USA", "China", "Japan", "Russia"}
+    String b[] = {};
+    char c[];
+
+//for(
+//    int i = 0;
+//    i<length of
+//    a[i];i++)
+//
+//    {
+//        c[] =c[].charAt(i)
+//
+//        String b[] = c[]+b[]
+//
+//    }
+//
+//System.out.println("Revese of array is"+b[]);
+//}
+//
+//}
